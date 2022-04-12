d7bc5225913bcf752f0a2c3612c97b583d856ee0
Kyselov-Vladyslav <66912506+Kyselov-Vladyslav@users.noreply.github.com>
2022-04-13 01:49:32 +0200

    wersja-test

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".btn");
+const display_none = document.querySelector(".display_none");
+const code = document.getElementById("code");
+
+
+btn.addEventListener("click", btnClick);
+
+function btnClick() {
+    if (code.value == "DELL/1/22") {
+        alert("Urządzenie dodane");
+        console.log(code.value)
+        
+    } else {
+        alert("Brak urządzenia w bazie, wpisz swoją uwagę");
+        display_none.classList.remove("display_none");
+        console.log(code.value)
+    }
+}
